e82f9e89d066606b4380dde3d793cacdd4902556
AnweshaPaul12 <anwesha@bdipo.com>
2018-01-29 15:46:19 +0600

    alignment issues are fixed

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function($){
+	$('.slider-item').slick({
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		autoplaySpeed: 5000,
+		autoplay: true,
+		cssEase: 'linear',
+		nextArrow: '.slider-nav .next',
+		prevArrow: '.slider-nav .prev',
+	});
+	$('.facebook-analysis-heading span').click(function(){
+		$('.facebook-analysis .form-block').slideDown("slow");
+		$('.facebook-analysis').css("padding-bottom", "110px");
+
+	});    
+	$('.facebook-analysis .form-block span').click(function(){
+		$('.facebook-analysis .form-block').slideUp("slow");
+
+		$('.facebook-analysis').css("padding-bottom", "0px");
+
+	});
+	$(window).scroll(function(){
+		var sticky = $('.second-header .navbar-default'),
+		scroll = $(window).scrollTop();
+
+		if (scroll >= 100){ 
+			sticky.addClass('fixed');
+		}
+		else {sticky.removeClass('fixed');
+	}
+});
+	$('[data-sidenav]').sidenav();
+
+});
